cbb9c1eebc844cc59c2e83845f3e5d5b42394ad7
sezerdogru <sezer.dogru91@gmail.com>
2022-06-07 21:16:56 +0300

    Add Post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
+import Post from '../Post/Post'
 import './Feed.css'
 
 function Feed  ( )  {
@@ -8,6 +9,34 @@ function Feed  ( )  {
     <div className="feed"> 
     <StoryReel />
     <MessageSender />
+    <Post 
+      profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      username="adsasad"
+      timestamp="sad"
+      message="asd"
+    />
+    <Post 
+      profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      username="adsasad"
+      timestamp="sad"
+      message="asd"
+    />
+    <Post 
+      profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      username="adsasad"
+      timestamp="sad"
+      message="asd"
+    />
+    <Post 
+      profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnnu6obVk9X7KpF7ddIVK0Xukk7GK5uWC1GA&usqp=CAU"
+      username="adsasad"
+      timestamp="sad"
+      message="asd"
+    />
 
     </div>
   )
