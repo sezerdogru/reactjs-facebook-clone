ae6af31360caff932f91a9b783d6bbc80e04ae69
sezerdogru <sezer.dogru91@gmail.com>
2022-06-07 20:20:50 +0300

    Add Feed

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import StoryReel from './StoryReel'
+import MessageSender from './MessageSender'
 import './Feed.css'
 
 function Feed  ( )  {
   return (
     <div className="feed"> 
     <StoryReel />
+    <MessageSender />
 
     </div>
   )
